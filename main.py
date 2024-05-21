8a45f746221608f314aef60ae570cd8bcf42608c
Harsha <harshahm0130@gmail.com>
2024-05-21 21:40:10 +0530

    Step-3.2

--- main.py
+++ main.py
@@ -10,6 +10,13 @@ scr.tracer(0)
 
 snake = Snake()
 
+scr.listen()
+scr.onkey(snake.up, "Up")
+scr.onkey(snake.down, "Down")
+scr.onkey(snake.right, "Right")
+scr.onkey(snake.left, "Left")
+
+
 game_is_on = True
 while game_is_on:
     scr.update()
